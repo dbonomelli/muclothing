239591645493c429905b0ed94f275bde33a6a2d3
Diego M. <diejamudelp@hotmail.com>
2023-09-23 20:58:56 -0300

    init

---
+++ src/main/java/cl/musolutions/muclothes/MuclothesApplication.java
@@ -0,0 +1,13 @@
+package cl.musolutions.muclothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuclothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuclothesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/musolutions/muclothes/MuclothesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.musolutions.muclothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuclothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
